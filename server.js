808e833b8de8082aed75f89b733a42317a6fce9e
Philippa Giibwa <p.giibwa@alustudent.com>
2025-03-22 17:32:57 +0200

    edited

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(morgan('dev'));
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
